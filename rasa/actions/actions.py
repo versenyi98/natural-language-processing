543a13ba8492f561f94e06a939e9b959e89438dd
versenyi98 <versenyi98@gmail.com>
2021-10-16 14:54:00 +0200

    Add name slot

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -3,9 +3,9 @@ from typing import Any, Text, Dict, List
 
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
+from rasa_sdk.events import SlotSet
 
-
-class ActionHelloWorld(Action):
+class ActionGetTime(Action):
 
     def name(self) -> Text:
         return "action_show_time"
@@ -16,4 +16,34 @@ class ActionHelloWorld(Action):
 
         dispatcher.utter_message(text=f"{dt.datetime.now()}")
 
+        return []
+
+class ActionReceiveName(Action):
+
+    def name(self) -> Text:
+        return "action_receive_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        text = tracker.latest_message['text']
+        dispatcher.utter_message(text=f"I'll remember your name {text}!")
+        return [SlotSet("name", text)]
+
+
+class ActionSayName(Action):
+
+    def name(self) -> Text:
+        return "action_say_name"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        name = tracker.get_slot("name")
+        if not name:
+            dispatcher.utter_message(text="I don't know your name.")
+        else:
+            dispatcher.utter_message(text=f"Your name is {name}!")
         return []
