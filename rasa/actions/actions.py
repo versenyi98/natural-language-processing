807d3d2145b37f9b180781cb66a2152b205d316a
versenyi98 <versenyi98@gmail.com>
2022-04-27 20:56:02 +0200

    Use hungarian text with the chatbot

--- rasa/actions/actions.py
+++ rasa/actions/actions.py
@@ -1,7 +1,8 @@
+from dis import dis
 from glob import glob
-from typing import Optional, Dict, Text, Any, List
+from typing import Optional, Dict, Text, Any
 
-import random
+import numpy as np
 
 from rasa_sdk import Tracker, Action
 from rasa_sdk.executor import CollectingDispatcher
@@ -9,16 +10,61 @@ from rasa_sdk.forms import FormValidationAction
 from rasa_sdk.types import DomainDict
 from rasa_sdk.events import SlotSet
 
+from unidecode import unidecode
+
+import os
+print(os.getcwd())
+
+# BERT
+import torch
+from transformers import BertTokenizerFast, BertForSequenceClassification
+
+tokenizer = BertTokenizerFast.from_pretrained('SZTAKI-HLT/hubert-base-cc')
+
+model = BertForSequenceClassification.from_pretrained(
+    'SZTAKI-HLT/hubert-base-cc',
+    num_labels = 4,
+    output_attentions=False,
+    output_hidden_states=False   
+)
+
+model.load_state_dict(torch.load('../.model_storage/BERT/model_SZTAKI-HLT_hubert-base-cc_long_question.pth'))
+
+target_names = ['Egészség', 'Szórakozás', 'Állatok', 'Számítástechnika']
+
+prediction = None
+
+def predict_category(question):
+    encoded_question = tokenizer.encode_plus(
+        question,
+        max_length = 128,
+        padding='max_length',
+        truncation=True,
+        return_token_type_ids=False
+    )
+
+    inputs = {
+        'input_ids':      torch.tensor(encoded_question['input_ids']).unsqueeze(0),
+        'attention_mask': torch.tensor(encoded_question['attention_mask']).unsqueeze(0),
+        'labels':         torch.tensor(0).unsqueeze(0),
+    }
+        
+    with torch.no_grad():
+        outputs = model(**inputs)
+        
+    logits = outputs[1]
+
+    pred = np.argmax(logits, axis=1).flatten().item()
+
+    return target_names[pred]
+
+# Elasticsearch
 from elasticsearch import Elasticsearch
-from elasticsearch_dsl import Search, Q
-from elasticsearch.helpers import bulk
-from elasticsearch.exceptions import ElasticsearchWarning
 
 ES_QUERY_SIZE = 20
 query_result = None
 answer_idx = -1
 
-# create elasticsearch client
 es = Elasticsearch("http://localhost:9200")
 
 def es_query(question):
@@ -33,38 +79,16 @@ def es_query(question):
         }
     }
 
-def find_next_answer(question):
+def find_next_answer(index, question):
     global query_result
     global answer_idx
 
     if query_result == None:
         body = es_query(question)
-        query_result = es.search(index="questions*", body=body)
+        query_result = es.search(index=index, body=body)
 
     answer_idx += 1
     return query_result['hits']['hits'][answer_idx]['_source']['answer']
-
-class ValidateIndexName(FormValidationAction):
-    def name(self) -> Text:
-        return "validate_index_name_form"
-
-    def validate_index_name(
-        self,
-        slot_value: Any,
-        dispatcher: CollectingDispatcher,
-        tracker: Tracker,
-        domain: DomainDict,
-    ) -> Dict[Text, Any]:
-        intent = tracker.latest_message['intent'].get('name')
-
-        index_name = None
-
-        if intent == 'agree':
-            index_name = 'question_category'
-        elif intent == 'disagree':
-            index_name = 'question_*'
-
-        return {"index_name": index_name}
 
 class CleanSlots(Action):
     def name(self) -> Text:
@@ -79,16 +103,37 @@ class CleanSlots(Action):
 
         return [SlotSet("question", None), SlotSet("index_name", None), SlotSet("satisfied", None)]
 
+class ValidateIndexName(FormValidationAction):
+    def name(self) -> Text:
+        return "validate_index_name_form"
+
+    def validate_index_name(
+        self,
+        slot_value: Any,
+        dispatcher: CollectingDispatcher,
+        tracker: Tracker,
+        domain: DomainDict,
+    ) -> Dict[Text, Any]:
+        intent = tracker.latest_message['intent'].get('name')
+
+        index_name = 'questions_'
+
+        if intent == 'agree':
+            global prediction
+            index_name += unidecode(prediction.lower())
+        elif intent == 'disagree':
+            index_name += '*'
+
+        return {"index_name": index_name}
+
 class PredictCategory(Action):
     def name(self) -> Text:
         return "predict_category"
     
     def run(self, dispatcher, tracker, domain):
-        categories = ["Számítástechnika", "Egészség", "Állatok", "Szórakozás"]
-
-        predicion = random.choice(categories)
-
-        dispatcher.utter_message(f"The predicted category is '{predicion}'\n")
+        global prediction
+        prediction = predict_category(tracker.get_slot('question'))
+        dispatcher.utter_message(f"Szerintem érdemes lenne csak a(z) '{prediction}' kategóriában keresni!\n")
         return []
 
 class GiveAnswer(Action):
@@ -96,7 +141,7 @@ class GiveAnswer(Action):
         return "give_answer"
     
     def run(self, dispatcher, tracker, domain):
-        dispatcher.utter_message(text=find_next_answer(tracker.get_slot('question')))
+        dispatcher.utter_message(text=find_next_answer(tracker.get_slot('index_name'), tracker.get_slot('question')))
         return []
 
 class ValidateSatisfied(FormValidationAction):
@@ -117,13 +162,15 @@ class ValidateSatisfied(FormValidationAction):
 
         if intent == 'agree':
             satisfied = 'yes'
+            dispatcher.utter_message(text="Örülök hogy segíthettem")
         else:
             # Finding next answer
             global answer_idx
 
             if answer_idx == ES_QUERY_SIZE - 1:
                 satisfied = 'no'
+                dispatcher.utter_message(text="Sajnálom hogy nem sikerült segíteni")
             else:
-                dispatcher.utter_message(text=find_next_answer(tracker.get_slot('question')))
+                dispatcher.utter_message(text=find_next_answer(tracker.get_slot('index_name'), tracker.get_slot('question')))
 
         return {"satisfied": satisfied}
